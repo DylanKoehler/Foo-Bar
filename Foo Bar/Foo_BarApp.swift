61863143f0f2f36c6396a915c7d3e6c4096b84b8
DylanKoehler <dylankoehler3212@gmail.com>
2022-08-26 12:56:09 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Dylan Koehler on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
